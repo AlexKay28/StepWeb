9ef8a10a503f7773e72eabd0e48feeb74d8be7ab
AlexKay28 <Kaygorodow@mail.ru>
2020-08-20 15:05:11 +0300

    tesk2

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    answer = bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")
-    return [answer]
+from cgi import parse_qs
+
+def application(environ, start_response):
+
+  query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+  body = []
+  for key, values in query.items():
+    for item in values:
+      body.append(key + "=" + item + "\r\n")
+   
+  status = '200 OK'
+  headers = [
+   ('Content-Type', 'text/plain')
+  ]
+  
+  start_response(status, headers)
+  return body
