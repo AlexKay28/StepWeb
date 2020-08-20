f2dd564822750f69901bd6281e20f871d3e23f75
AlexKay28 <Kaygorodow@mail.ru>
2020-08-20 13:17:41 +0300

    gg

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql star
+﻿sudo /etc/init.d/mysql start﻿
