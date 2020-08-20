b614f80735ab5e0c8d71d6a7a9708d1ab8346c25
AlexKay28 <Kaygorodow@mail.ru>
2020-08-20 12:54:26 +0300

    eh

--- init.sh
+++
@@ -1,5 +0,0 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
