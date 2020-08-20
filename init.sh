0c9398cae063d78f3150697895af464a28e94db0
AlexKay28 <Kaygorodow@mail.ru>
2020-08-20 13:25:29 +0300

    target

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
